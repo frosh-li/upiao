2016cd70bf9d1c016b463c8460852639bf4dc710
李俊良 <lijunliang6@wanda.cn>
2016-10-19 17:16:44 +0800

    update client

--- client/data_tpl/group.js
+++ client/data_tpl/group.js
@@ -4,8 +4,13 @@ var data = `{
     "CurSensor": 1,
     "GroBats":6,
     "Current": {float|13.24|10},
+    "CurCol":{array|[0,1,2,3]},
     "Voltage": {float|127.06|10},
-    "ChaState": {array|[0,1,2]}
+    "VolCol":{array|[0,1,2,3]},
+    "ChaState": {array|[0,1,2]},
+    "Avg_R":{float|1.3|2},
+    "Avg_U":{float|10|10},
+    "Avg_T":{float|30|30}
 }`;
 
 module.exports = data;
--- client/data_tpl/station.js
+++ client/data_tpl/station.js
@@ -10,9 +10,13 @@ var data = `{
     "Groups":2,
     "GroBats":6,
     "Current": {float|25.03|5},
+    "CurCol":{array|[0,1,2,3]},
     "Voltage": {float|127.38|100},
+    "VolCol":{array|[0,1,2,3]},
     "Temperature": {float|34.2|30},
+    "TemCol":{array|[0,1,2,3]},
     "Humidity": {int|78|20},
+    "HumCol":{array|[0,1,2,3]},
     "ChaState": {array|[0,1,2]},
     "Lifetime":{int|50|50},
     "Capacity":{int|50|50}
