89287a1be3fa19f6e3572e124a2b9451cb114060
qingda <qingda@qq.com>
2016-11-06 16:28:40 +0800

    watch now

--- common/watchSite.js
+++ common/watchSite.js
@@ -2,9 +2,9 @@ function onConnectSite(sn_key){
     var sql = `delete from systemAlarm where station=${sn_key}`;
 	conn.query(sql, function(err, results){
 		if(err){
-			console.log('update system alarm done', err);
+			console.log('update system alarm fail', err);
 		}else{
-			console.log('update system alarm fail');
+			console.log('update system alarm done');
 		}
 
 		//insertBulkHistory(data, cb);
@@ -18,9 +18,35 @@ function disConnectSite(sn_key){
     console.log(sql);
 	conn.query(sql, function(err, results){
 		if(err){
-			console.log('update system alarm done', err);
+			console.log('update system alarm fail', err);
 		}else{
-			console.log('update system alarm fail');
+			console.log('update system alarm done');
+		}
+	})
+
+	// 去掉站点消息
+
+	conn.query(`delete from tb_station_module where sn_key=${sn_key}`, function(err, data){
+		if(err){
+			console.log('remote from tb_station_module err', err);
+		}else{
+			console.log('remote from tb_station_module done');
+		}
+	})
+
+	conn.query(`delete from tb_group_module where floor(sn_key/10000) = ${sn_key/10000}`, function(err, data){
+		if(err){
+			console.log('remote from tb_group_module err', err);
+		}else{
+			console.log('remote from tb_group_module done');
+		}
+	})
+
+	conn.query(`delete from tb_battery_module where floor(sn_key/10000) = ${sn_key/10000}`, function(err, data){
+		if(err){
+			console.log('remote from tb_battery_module err', err);
+		}else{
+			console.log('remote from tb_battery_module done');
 		}
 	})
 }
