69ad51d621d704ccdcc2a23dabeb3591407e6c9e
lijunliang <junguoliang@163.com>
2017-07-04 15:00:17 +0800

    清理不在my site表中的数据

--- tcpserver.js
+++ tcpserver.js
@@ -102,6 +102,9 @@ function clearSites(){
 	conn.query('delete from tb_group_module where record_time'+"<'"+nowString+"'");
 	conn.query('delete from tb_battery_module where record_time'+"<'"+nowString+"'");
 	conn.query('update my_alerts set status=4, markup="系统自动处理", markuptime="'+currentDateStr+'" where time<"'+nowClearCautionString+'"')
+
+	// 清理系统报警
+	conn.query('delete from systemalarm where station not in (select serial_number from my_site)');
 }
 
 setInterval(clearSites,5000);
