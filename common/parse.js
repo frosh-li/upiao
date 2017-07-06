b95317bc4daeb42eab4630a68fb3b776d4b0ed48
lijunliang <junguoliang@163.com>
2017-07-06 14:43:05 +0800

    自动清理的警情的问题

--- common/parse.js
+++ common/parse.js
@@ -184,7 +184,7 @@ function insertErrorBulk(data){
 			return new Promise(function(resolve, reject){
 				var sql = `
 					select * from my_alerts where
-					status != 2 and
+					status != 2 and status != 4 and
 					sn_key = '${item.sn_key}' and
 					code = '${item.code}'
 				`;
