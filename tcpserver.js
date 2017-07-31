6144bac84d22feac3d6f98746f03e98b30ee24c5
lijunliang <junguoliang@163.com>
2017-07-31 16:22:06 +0800

    sn_key to string

--- tcpserver.js
+++ tcpserver.js
@@ -128,7 +128,7 @@ function syncParams(){
     conn.query(sql, (err, results)=>{
         results.forEach((item)=>{
             sendParamHard('StationPar', {
-                sn_key:item.sn_key.toString,
+                sn_key:item.sn_key.toString(),
                 CurSensor: item.CurSensor
             });
         });
