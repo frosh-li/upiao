d96c42bf73075f97239c63dc7028176600a7b05a
li <lijunliang@hfbank.com.cn>
2018-06-17 13:33:45 +0800

    自动清理报警

--- common/watchSite.js
+++ common/watchSite.js
@@ -101,17 +101,12 @@ function clearSites(){
 	conn.query('delete from tb_station_module where record_time'+"<'"+nowString+"'");
 	conn.query('delete from tb_group_module where record_time'+"<'"+nowString+"'");
 	conn.query('delete from tb_battery_module where record_time'+"<'"+nowString+"'");
-	var clearSql = `update
+	var clearSql = `delete
                   my_alerts
                   set
-                  status=4,
-                  markup="系统自动处理",
-                  markuptime="${currentDateStr}"
                   where
                   time<"${currentClearDateStr}"
-                  and
-                  status=0`;
-  logger.info('clear nowCaution', clearSql);
+                  `;
 	conn.query(clearSql);
 
 	// 清理系统报警
